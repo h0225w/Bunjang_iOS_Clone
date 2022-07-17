2ff178b4d2b7a1863890ea652a440cad324b9ace
h0225w <gusdn5387@naver.com>
2022-07-17 15:03:40 +0900

    홈 > 스크롤뷰 생성

--- Bunjang/UserInterfaces/Home/HomeView.swift
+++ Bunjang/UserInterfaces/Home/HomeView.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class HomeView: UIViewController {
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupNavigationBar()
+        setupViews()
     }
 }
 
 // MARK: - Extension
-private extension MyView {
+private extension HomeView {
+    // MARK: 네비게이션 바 설정
+    func setupNavigationBar() {
+        let standardAppearance = UINavigationBarAppearance()
+        standardAppearance.configureWithOpaqueBackground()
+        standardAppearance.backgroundColor = .white
+        standardAppearance.titleTextAttributes = [.foregroundColor: UIColor.black]
+        
+        let scrollEdgeAppearance = UINavigationBarAppearance()
+        scrollEdgeAppearance.configureWithTransparentBackground()
+
+        navigationController?.navigationBar.standardAppearance = standardAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = scrollEdgeAppearance
+    }
     
+    // MARK: 뷰 설정
+    func setupViews() {
+        scrollView.delegate = self
+    }
+}
+
+extension HomeView: UIScrollViewDelegate {
+    // TODO: 스크롤 했을 때 NavigationBar tint color 바꾸는 방법
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollView.contentOffset.y > 0 {
+            navigationController?.navigationBar.tintColor = .black
+        } else {
+            navigationController?.navigationBar.tintColor = .white
+        }
+    }
 }
